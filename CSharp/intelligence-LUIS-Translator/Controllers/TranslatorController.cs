a200a1c2f25becb4e291a1c2984c66faf16f2340
Ahmed Magdy <ahmed.magdy.g.s@gmail.com>
2018-09-18 14:47:30 +0200

    Initial sample skeleton

---
+++ CSharp/intelligence-LUIS-Translator/Controllers/TranslatorController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Bot.Builder.AI.Translation;
+using Microsoft.Bot.Builder.AI.Translation.PostProcessor;
+using Microsoft.Bot.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Configuration;
+
+namespace LuisBot.Controllers
+{
+    public class TranslatorController
+    {
+        private ITranslator translator;
+        public TranslatorController()
+        {
+            translator = new Translator(WebConfigurationManager.AppSettings["translatorKey"]);
+        }
+
+        public async Task TranslateActivityText(Activity activity, string targetLanguage)
+        {
+            string sourceLanguage = await DetectLanguageAsync(activity.Text);
+            ITranslatedDocument translatedDocument = await TranslateAsync(activity.Text, sourceLanguage, targetLanguage);
+            activity.Text = translatedDocument.GetTranslatedMessage();
+        }
+        
+        private async Task<string> DetectLanguageAsync(string textToDetect)
+        {
+            return await translator.DetectAsync(textToDetect);
+        }
+
+        private async Task<ITranslatedDocument> TranslateAsync(string textToTrasnlate, string from, string to)
+        {
+            return await translator.TranslateAsync(textToTrasnlate, from, to);
+        }
+
+        private async Task<List<ITranslatedDocument>> TranslateArrayAsync(string[] textToTrasnlate, string from, string to)
+        {
+            return await translator.TranslateArrayAsync(textToTrasnlate, from, to);
+        }
+
+    }
+}
---
+++ CSharp/intelligence-LUIS-Translator/Dialogs/RootLuisDialog.cs
@@ -0,0 +1,83 @@
+﻿namespace LuisBot.Dialogs
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Web;
+    using Microsoft.Bot.Builder.Dialogs;
+    using Microsoft.Bot.Builder.FormFlow;
+    using Microsoft.Bot.Builder.Luis;
+    using Microsoft.Bot.Builder.Luis.Models;
+    using Microsoft.Bot.Connector;
+
+    [Serializable]
+    public class RootLuisDialog : LuisDialog<object>
+    {
+        private const string EntityHotelName = "Hotel";
+
+        private const string EntitiyLocation = "Location";
+
+        private const string EntityName = "Name";
+
+        public RootLuisDialog() : base(new LuisService(new LuisModelAttribute(
+            ConfigurationManager.AppSettings["LuisAppId"],
+            ConfigurationManager.AppSettings["LuisSubscriptionKey"],
+            domain: ConfigurationManager.AppSettings["LuisAPIHostName"])))
+        {
+        }
+
+        [LuisIntent("")]
+        [LuisIntent("None")]
+        public async Task None(IDialogContext context, LuisResult result)
+        {
+            await this.ShowTranslation(context, result);
+            await this.ShowLuisResult(context, result);
+        }
+
+        [LuisIntent("SearchHotels")]
+        public async Task Search(IDialogContext context, IAwaitable<IMessageActivity> activity, LuisResult result)
+        {
+            await this.ShowTranslation(context, result);
+            await this.ShowLuisResult(context, result);
+
+        }
+
+        [LuisIntent("ShowHotelsReviews")]
+        public async Task Reviews(IDialogContext context, LuisResult result)
+        {
+            await this.ShowTranslation(context, result);
+            await this.ShowLuisResult(context, result);
+        }
+
+        [LuisIntent("Help")]
+        public async Task Help(IDialogContext context, LuisResult result)
+        {
+            await this.ShowTranslation(context, result);
+            await this.ShowLuisResult(context, result);
+        }
+
+        [LuisIntent("Greeting")]
+        public async Task Greeting(IDialogContext context, LuisResult result)
+        {
+            await this.ShowTranslation(context, result);
+            await this.ShowLuisResult(context, result);
+        }
+
+        private async Task ShowTranslation(IDialogContext context, LuisResult result)
+        {
+            string translationMessage = "Your message after translation is: " + result.Query;
+            await context.PostAsync(translationMessage);
+            context.Wait(MessageReceived);
+        }
+
+
+        private async Task ShowLuisResult(IDialogContext context, LuisResult result)
+        {
+            string luisMessage = "Your top intent was is: " + result.Intents[0].Intent + " with score: " + result.Intents[0].Score.ToString();
+            await context.PostAsync(luisMessage);
+            context.Wait(MessageReceived);
+        }
+    }
+}
